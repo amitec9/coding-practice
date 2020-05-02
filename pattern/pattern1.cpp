0fe29b378e178c8f32463b1a6355d5e28d92b6a3
Amit kumar <eramit3096@gmail.com>
2020-05-02 10:31:17 +0530

    Your branch is up to date with 'origin/master'.

---
+++ pattern/pattern1.cpp
@@ -0,0 +1,18 @@
+//pattern \
+//Author :- Amit kumar
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	for(int i =0; i<=5;i++)
+	{
+		for(int j =0; j<i;j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+		
+	}
+}
