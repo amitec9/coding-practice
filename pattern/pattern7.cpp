95dd595aea58427bf33d146b81f725079fe23af1
Amit kumar <eramit3096@gmail.com>
2020-05-11 00:04:22 +0530

    Your branch is up to date with 'origin/master'.

--- pattern/pattern7.cpp
+++ pattern/pattern7.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 int main()
 {
-    for(int i=1;i<=5;i++)
+	  for(int i=1;i<=5;i++)
     {
         for(int k=1;k<=5-i;k++)
             cout<<" ";
@@ -20,7 +20,19 @@ int main()
        cout<<endl;
     }
 	
+	int i,j,k;
+     for( i=1;i<=5;i++)
+    {
+        for( k=1;k<=i-1;k++)
+            cout<<" ";
+        
+        for( j=1; j<=(5*2-(2*i-1)); j++)
+        {
+            
+            cout<<j;
+        }
+        
+       cout<<endl;
+    }
 	
-
-   
-} 
+}
--- pattern/pattern8.cpp
+++ pattern/pattern8.cpp
@@ -1,12 +1,8 @@
-/* pattern */
-
-
-
 #include<iostream>
 using namespace std;
 int main()
 {
-    for(int i=1;i<=5;i++)
+	  for(int i=1;i<=5;i++)
     {
         for(int k=1;k<=5-i;k++)
             cout<<" ";
@@ -14,13 +10,16 @@ int main()
         for(int j=1; j<=2*i-1; j++)
         {
             
-            cout<<j;
+             if(i==5 || j==1 || j==(2*i-1))
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
         }
         
        cout<<endl;
     }
-	
-	
-
-   
-} 
+	}
