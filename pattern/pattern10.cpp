c9bde35a69b13eadac51f4407a18f28e166d8ce5
Amit kumar <eramit3096@gmail.com>
2020-05-13 00:16:04 +0530

    Your branch is up to date with 'origin/master'.

---
+++ pattern/pattern10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int count=0;
+	
+     for( int i=1;i<5;i++)
+    {
+		for(int j=1; j<5;j++)
+		{
+			count++;
+			cout<<count<<" ";
+		}
+        
+       cout<<endl;
+    }
+}
