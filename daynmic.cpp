2ec4fd6ebb3f62d2a29dfbfc7db1ecdaf40629eb
Amit kumar <49529841+amitec9@users.noreply.github.com>
2020-08-05 20:23:51 +0530

    Add files via upload

---
+++ daynmic.cpp
@@ -0,0 +1,18 @@
+//pointer \
+//Author :- Amit kumar
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	int *p;
+	p=&a;
+	cout<<*p<<endl;
+	cout<<&p<<endl;
+	cout<<&a<<endl;
+	cout<<a<<endl;
+	cout<<p<<endl;
+	
+	
+}
