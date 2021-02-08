50c1d1042a39d2988c3ab16136004543f7ae6bfa
Amit kumar <49529841+amitec9@users.noreply.github.com>
2021-02-08 23:51:47 +0530

    Add files via upload

---
+++ Arr_rev.cpp
@@ -0,0 +1,12 @@
+//Write a program to reverse an array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[ ]={1,5,3,7,2};
+    for(int i=4;i>=0;i--)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+}
